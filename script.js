2dfa005d951afc34859d035bdb782a54214a80ea
namHN <namhn201@gmail.com>
2024-09-16 09:58:32 +0700

    xu ly form dangky

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //reponsive
 function toggleMenu() {
     var menu = document.querySelector('.menu');
@@ -48,12 +49,27 @@ document.getElementById('accountName').onclick = function() {
     }
 };
 
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function(e) {
     var accountContainer = document.querySelector('.account-container');
-    if (!accountContainer.contains(event.target)) {
+    if (!accountContainer.contains(e.target)) {
         document.getElementById('accountOptions').style.display = 'none';
     }
 });
+
+//Sign_Up
+document.querySelector('.sign_up').onclick = function(){
+    document.querySelector('.signup_popup').style.display = 'block'
+}
+document.querySelector('.signup_popup').onclick = function(e){
+    const form = document.querySelector('.form')
+    if(!form.contains(e.target)){
+        document.querySelector('.signup_popup').style.display = 'none'
+
+    }
+    
+}
+
+
 
 document.getElementById('logoutButton').onclick = function() {
     document.getElementById('loginButton').style.display = 'block';
@@ -98,3 +114,178 @@ document.querySelector('#button_get_2_main').onclick = function(){
 document.querySelector('#button_get_3_main').onclick = function(){
     alert('Comming Soon')
 }
+//Slider
+const card = document.querySelector('.card');
+let currentIndex = 0;
+
+document.getElementById('next').addEventListener('click', function() {
+    console.log("NEXT CHƯA")
+    if (currentIndex < 3) { // Có 3 slides nên tối đa là 2
+        currentIndex++;
+    } else {
+        console.log("current index ", currentIndex);
+        
+        currentIndex = 0;
+    }
+    card.style.transform = `translateX(-${currentIndex * 33.33}%)`;
+});
+
+document.getElementById('prev').addEventListener('click', function() {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = 2;
+    }
+    card.style.transform = `translateX(-${currentIndex * 33.33}%)`;
+});
+
+
+///Validator
+
+function Validator(option){
+    var option = option
+    var formElement = document.querySelector(option.form)
+    console.log(option.rule)
+
+    function messageErrol(inputElement, rule) {
+        var errormessage = inputElement.parentElement.querySelector(option.errormessage);
+        var isError;
+    
+        if (typeof rule.text === 'function') {
+            isError = rule.text(inputElement.value);
+        }else {
+            isError = 'Invalid rule';
+        }
+
+    
+        if (isError) {
+            errormessage.innerHTML = isError;
+            inputElement.parentElement.classList.add("invalid");
+        } else {
+            errormessage.innerHTML = '';
+            inputElement.parentElement.classList.remove("invalid");
+        }
+        return isError;
+    }
+    
+    
+    if(formElement){
+
+        formElement.onsubmit = function(e) {
+            e.preventDefault(); 
+            var isFormValid = true; 
+            var formData = {};
+        
+            option.rule.forEach(rule => {
+                var inputElement = document.querySelector(rule.selector);
+                var isValid;
+        
+                if (rule.confirmPassword) {
+                    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+                    console.log("GÌ đây",rule.confirmPassword)
+                    isValid = rule.confirmPassword(inputElement.value);
+                    if (isValid) {
+                        var confirmPasswordElement = document.querySelector(rule.selector);
+                        var errormessage = confirmPasswordElement.parentElement.querySelector(option.errormessage);
+        
+                        errormessage.innerHTML = isValid;
+                        confirmPasswordElement.parentElement.classList.add('invalid');
+                        isFormValid = false;
+                    } else {
+                        var confirmPasswordElement = document.querySelector(rule.selector);
+                        var errormessage = confirmPasswordElement.parentElement.querySelector(option.errormessage);
+        
+                        errormessage.innerHTML = '';
+                        confirmPasswordElement.parentElement.classList.remove('invalid');
+                    }
+                } else {
+                    console.log("BBBBBBBBBBBBBBBBBBBBBBBBBBBB")
+                    isValid = messageErrol(inputElement, rule);
+                    if (isValid) {
+                        isFormValid = false;
+                    }
+                    console.log("isValid", isValid)
+
+                    console.log("isFormValid", isFormValid)
+                }
+        
+                var inputName = inputElement.name;
+                var inputValue = inputElement.value;
+                formData[inputName] = inputValue;
+            });
+        
+            if (isFormValid) {
+                console.log('Form is valid');
+                document.querySelector('.signup_popup').style.display = 'none';
+                var accountName = document.querySelector('#accountName')
+                accountName.innerHTML = formData.fullname
+
+            } else {
+                console.log('Form is not valid');
+            }
+        };
+        
+    }
+
+    option.rule.forEach( rule => {
+        console.log("rule selector",rule.selector)
+        var inputElement = document.querySelector(rule.selector)
+        console.log("INput",inputElement)
+        //Xử lý blur
+        inputElement.onblur = function(){
+            console.log("BLUR input:", inputElement)
+        messageErrol(inputElement, rule)
+
+        }
+        inputElement.addEventListener('oninput', function() {
+            messageErrol(inputElement, rule);
+        });
+        
+        
+    });
+
+    // console.log(formElement)
+    // var formSelector = formElement.document.querySelector(option.selector)
+    // console.log(formSelector)
+
+}
+
+Validator.isRequired = function(selector){
+    // console.log(selector);
+    return{
+        selector: selector,
+        text: function(value){
+            return value.trim() ? null : 'Please enter this field'
+        }
+    }
+}
+Validator.isMail = function(selector){
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return {
+        selector: selector,
+        text: function(value){
+            return mailformat.test(value) ? undefined : 'Please enter mail'
+
+            
+        }
+    };}
+Validator.isPassword = function(selector){
+    console.log(selector);
+    return{
+        selector: selector,
+        text: function(value){
+            return value.trim() ? null : 'At least 8 chars, 1 uppercase, 1 lowercase, 1 digit.'
+        }
+    }
+}
+Validator.isPasswordConfirm = function(selector, getvalue){
+    console.log("getvalue", getvalue)
+    return {
+        selector: selector,
+        text: function(value){
+            return value === getvalue() ? undefined : 'Wrong password'
+
+        }
+    };}
+
+
